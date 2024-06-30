af87e8a67d493a6b49c931a928de13d85d818456
SavitarN <nischalzoc999@gmail.com>
2024-06-30 23:14:09 +0545

    Nike page

---
+++ project-1/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
---
+++ project-1/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="../images/brand_logo.png" alt="logo" />
+        </div>
+
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+}
